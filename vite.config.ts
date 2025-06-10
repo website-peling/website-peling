c782d2c06124095a39274b1dcb81d6da721e219e
website-peling <juki123haa@gmail.com>
2025-06-10 11:50:50 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website-peling/',  // Ganti dengan nama repo GitHub kamu
   plugins: [react()],
-});
+  base: '/website-peling/', // ⬅️ wajib disesuaikan dengan nama repo
+})
